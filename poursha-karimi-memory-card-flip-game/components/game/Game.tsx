077d662e4fcebbef8eb6f3c84c2b1bf82cfb178c
poursha98 <ghostworrior8@gmail.com>
2025-12-05 00:06:05 +0330

    fixes

--- poursha-karimi-memory-card-flip-game/components/game/Game.tsx
+++ poursha-karimi-memory-card-flip-game/components/game/Game.tsx
@@ -2,15 +2,13 @@
 import { useGame } from "@/utils/GameContext";
 import StartScreen from "@/components/start-screen/StartScreen";
 import GameBoard from "@/components/game-board/GameBoard";
-// import { useEffect } from "react";
+const whilePlaying = ["in_progress", "comparing", "finished"];
 export default function Game() {
   const { status } = useGame();
   return (
     <main className="">
       {status === "ready" && <StartScreen />}
-      {(status === "in_progress" ||
-        status === "comparing" ||
-        status === "finished") && <GameBoard />}
+      {whilePlaying.includes(status) && <GameBoard />}
     </main>
   );
 }
