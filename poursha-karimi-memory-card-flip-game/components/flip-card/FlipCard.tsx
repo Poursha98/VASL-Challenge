3aa537af3b4739507328884915b435c5b25e08ee
poursha98 <ghostworrior8@gmail.com>
2025-12-06 00:43:17 +0330

    flip animation for cards

--- poursha-karimi-memory-card-flip-game/components/flip-card/FlipCard.tsx
+++ poursha-karimi-memory-card-flip-game/components/flip-card/FlipCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardFooter } from "@/components/ui/card";
 import { useGame } from "@/utils/GameContext";
 import { GameCard } from "@/types/gameTypes";
 import { hexToRGBA } from "@/utils/hexToRGBA";
+import { FileQuestionMark } from "lucide-react";
 export default function FlipCard({ card }: { card: GameCard }) {
   const Icon = card.icon;
   const { status, flipped, match, dispatch } = useGame();
@@ -13,36 +14,51 @@ export default function FlipCard({ card }: { card: GameCard }) {
   }
   return (
     <Card
-      className={`bg-gray-400 w-full h-full py-0 shadow-none gap-0 rounded-3xl  flex items-center justify-center cursor-pointer`}
+      className={` w-full h-full p-0 shadow-none border-none bg-transparent gap-0 flex items-center justify-center cursor-pointer perspective-[1000]`}
       onClick={handleClick}
-      style={
-        isMatchedCard ? { backgroundColor: hexToRGBA(card.fill, 0.5) } : {}
-      }
     >
-      {flipped.includes(card.id) || match.includes(card.id) ? (
-        <Icon fill={card.fill} className="size-18" strokeWidth={0.6} />
-      ) : (
-        <span
-          className={`${
-            isMatchedCard || flipped.includes(card.id)
-              ? "text-gray-100"
-              : "text-gray-400"
+      <div
+        className={`relative w-full h-full transition-all duration-500 transform-3d ${
+          flipped.includes(card.id) || isMatchedCard
+            ? "transform-[rotateY(180deg)]"
+            : ""
+        }`}
+      >
+        <div
+          className={`absolute inset-0 w-full h-full flex items-center justify-center bg-gray-400 rounded-3xl backface-hidden ${
+            isMatchedCard ? "opacity-0" : ""
           }`}
         >
-          {Array.from({ length: 5 }, () => "*").join("")}
-        </span>
-      )}
-      {status !== "finished" && (
-        <CardFooter
-          className={`${
+          <span className={`text-gray-400 flex items-center justify-center`}>
+            <FileQuestionMark
+              className="size-18"
+              strokeWidth={0.6}
+              stroke="#fff"
+            />
+          </span>
+        </div>
+        <div
+          className="absolute inset-0 w-full h-full flex flex-col items-center justify-center rounded-3xl backface-hidden transform-[rotateY(180deg)] "
+          style={
             isMatchedCard || flipped.includes(card.id)
-              ? "text-gray-100"
-              : "text-gray-400"
-          } sm:text-lg text-sm font-medium  `}
+              ? { backgroundColor: hexToRGBA(card.fill, 0.7) }
+              : {}
+          }
         >
-          {card.name}
-        </CardFooter>
-      )}
+          <Icon fill={card.fill} className="size-18" strokeWidth={0.6} />
+          {status !== "finished" && (
+            <CardFooter
+              className={`${
+                isMatchedCard || flipped.includes(card.id)
+                  ? "text-gray-800"
+                  : "text-gray-400"
+              } sm:text-lg text-sm font-medium absolute bottom-1 `}
+            >
+              {card.name}
+            </CardFooter>
+          )}
+        </div>
+      </div>
     </Card>
   );
 }
